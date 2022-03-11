6ab14f7a3c9d0b7094c01f0eee1ed856e36bcd1d
itsmenexus23 <87323493+itsmenexus23@users.noreply.github.com>
2022-03-11 22:57:08 +0800

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const client = new Discord.Client();
 const prefix = '$';
 
 client.once('ready', () => {
-    console.log('Samaritan#1544 Is Now Online! In Game HUB!');
+    console.log('BOT Is Now Online! In Game HUB!');
 });
 
 client.on ('message', message =>{
@@ -14,25 +14,25 @@ client.on ('message', message =>{
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
 
-    if(command === 'discinv'){  
-        message.channel.send('https://discord.io/gamingshub');
-    } else if (command == 'moaptwiton'){
-        message.channel.send('Moapilaw is now streaming at https://www.twitch.tv/moapilaw @everyone')
+    if(command === 'command here'){  
+        message.channel.send('message here');
+    } else if (command == 'command here'){
+        message.channel.send('message here')
     };
-    if(command === 'discbotmake1'){  
-        message.channel.send('Watch this video! Making Discord BOT! https://www.youtube.com/watch?v=j_sD9udZnCk');
-    } else if (command == 'mcip2'){
-        message.channel.send('')
+    if(command === 'command here'){  
+        message.channel.send('message here');
+    } else if (command == 'command here'){
+        message.channel.send('message here')
     }
-    if(command === 'mcaternos1'){  
-        message.channel.send('Paste in minecraft direct Connect: RediazDaulatDelacorg.aternos.me:13898');
-    } else if (command == 'ip'){
-        message.channel.send('CMD + ipconfig')
+    if(command === 'command here'){  
+        message.channel.send('message here');
+    } else if (command == 'command here'){
+        message.channel.send('message here')
     }     
-    if(command === 'btversion'){  
-        message.channel.send('Samaritan#1544 is on Version ' + Discord.version);
-    } else if (command == 'finnstron'){
-        message.channel.send('Its Just Me Nexus is now streaming at https://www.twitch.tv/itsmenexus23 @everyone')
+    if(command === 'command here'){  
+        message.channel.send('message here);
+    } else if (command == 'command here'){
+        message.channel.send('message here')
     }     
 });
 
